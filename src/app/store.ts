c81a0973a053a6d18411871485ff4f34fed20d9f
bombin78 <bombin78@mail.ru>
2024-03-31 23:18:51 +0600

    start project

---
+++ src/app/store.ts
@@ -0,0 +1,22 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {},
+    // middleware: (getDefaultMiddleware) => {
+    //   return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+    // },
+});
+
+export type AppStore = typeof store;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = AppStore["dispatch"];
+
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>;
